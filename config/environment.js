d8b8a447b303779ae9a1b2a9097138fb37d89136
Ivo Dejanovic <ivo.dejanovic@productive.io>
2021-10-31 19:54:41 +0100

    [feature] added typescript

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,7 @@ module.exports = function (environment) {
   let ENV = {
     modulePrefix: 'spotify',
     environment,
+    base: 'https://api.spotify.com', 
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -24,11 +25,6 @@ module.exports = function (environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
